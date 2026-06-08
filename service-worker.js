f08895775c72e874906c9cee42bf4118c707f41a
lastride01111-cpu <lastride01111@gmail.com>
2026-06-08 18:23:43 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'clinical-app-v4';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'manifest.json'
+];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// オフライン時はキャッシュからページを返す
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
